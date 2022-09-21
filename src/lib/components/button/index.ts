0f8133fbe4c7a07fa6871ebc8457036ee4786a3e
Craig Howell <craighowell@craigs-mbp.lan>
2022-09-20 20:40:40 -0400

    export fix attempt two

--- src/lib/components/button/index.ts
+++ src/lib/components/button/index.ts
@@ -2,14 +2,14 @@ import OriginalButton from './Button.svelte';
 import Group from './Group.svelte';
 import Loader from './Loader.svelte';
 
-// export interface ButtonStatic {
-// 	new (): OriginalButton;
-// 	Group: typeof Group;
-// 	Loader: typeof Loader;
-// }
+export interface ButtonStatic extends OriginalButton {
+	new (): OriginalButton;
+	Group: typeof Group;
+	Loader: typeof Loader;
+}
 
-// const Button = OriginalButton as ButtonStatic;
-const Button = OriginalButton;
+const Button = OriginalButton as ButtonStatic;
+// const Button = OriginalButton;
 Button.Group = Group;
 Button.Loader = Loader;
 
