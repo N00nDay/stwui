50db8b237ebbdfb53d2fc4ad2a15add373944b3c
Craig Howell <craighowell@craigs-mbp.lan>
2022-10-08 15:42:36 -0400

    docs: added copy button to slot tables

---
+++ src/docs/components/slots-table/CopyButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Card, Table, Button } from '../../../lib';
+	import { copyToClipboard } from '../../../lib/actions';
+
+	export let stringToCopy: string;
+
+	let copyState = false;
+
+	function onCopyClick() {
+		copyToClipboard(stringToCopy);
+		copyState = true;
+		setTimeout(() => {
+			copyState = false;
+		}, 2000);
+	}
+</script>
+
+<Button
+	on:click={onCopyClick}
+	size="xs"
+	class="ml-2 bg-light-icon-background dark:bg-dark-icon-background text-light-icon dark:text-dark-icon"
+>
+	{!copyState ? 'Copy' : 'Copied ✓'}
+</Button>
--- src/docs/components/slots-table/SlotsTable.svelte
+++ src/docs/components/slots-table/SlotsTable.svelte
@@ -2,6 +2,7 @@
 	import type { TableColumn } from '../../../lib/types/table-column';
 	import { Card, Table } from '../../../lib';
 	import type { Slot } from '../../types/slots-table';
+	import CopyButton from './CopyButton.svelte';
 
 	export let component: string;
 	export let slots: Slot[];
@@ -33,7 +34,11 @@
 					class="cursor-default hover:bg-light-surface dark:hover:bg-dark-surface"
 				>
 					<Table.Body.Row.Cell column={0}>{slot.slot}</Table.Body.Row.Cell>
-					<Table.Body.Row.Cell column={1}>{slot.component}</Table.Body.Row.Cell>
+					<Table.Body.Row.Cell column={1}
+						>{slot.component}{#if slot.component.length > 0}
+							<CopyButton stringToCopy={slot.component} />
+						{/if}</Table.Body.Row.Cell
+					>
 				</Table.Body.Row>
 			{/each}
 		</Table.Body>
