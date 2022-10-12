46fd7d81af3a923163115264ef419278ff21548a
Craig Howell <craighowell@craigs-mbp.lan>
2022-10-12 15:32:27 -0400

    docs: added clickOutside

---
+++ src/routes/click-outside/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Col } from '../../lib';
+	import { clickOutside } from '../../lib/actions';
+	import { example } from './examples';
+	import { CodeBlock } from '../../docs';
+
+	function handleClick() {
+		alert('Clicked outside!');
+	}
+</script>
+
+<Col class="col-24">
+	<div class="flex items-center justify-center p-12">
+		<button use:clickOutside={handleClick} class="bg-primary text-white rounded-md px-4 py-2">
+			Click Outside of Me
+		</button>
+	</div>
+
+	<br />
+
+	<CodeBlock language="svelte" code={example} />
+</Col>
---
+++ src/routes/click-outside/examples.ts
@@ -0,0 +1,14 @@
+export const example = `
+<script lang="ts">
+	import { clickOutside } from 'stwui/actions';
+
+   function handleClick() {
+		alert('Clicked outside!');
+	}
+</script>
+
+<div class="flex items-center justify-center p-12">
+   <button use:clickOutside={handleClick} class="bg-primary text-white rounded-md px-4 py-2">
+      Click Outside of Me
+   </button>
+</div>`;
