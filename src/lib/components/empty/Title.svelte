65e651bffa3b4de22bc6a218e7e8d6a112c5e254
Craig Howell <howell.craig@gmail.com>
2022-10-14 14:46:10 -0400

    fix(Empty): forward all events including child components

--- src/lib/components/empty/Title.svelte
+++ src/lib/components/empty/Title.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
 	import { twMerge } from 'tailwind-merge';
+	import { get_current_component } from 'svelte/internal';
+	import { forwardEventsBuilder, useActions, type ActionArray } from '../../actions';
+	export let use: ActionArray = [];
+	import { exclude } from '../../utils/exclude';
+	const forwardEvents = forwardEventsBuilder(get_current_component());
 
 	const defaultClass = 'mt-2 text-lg font-medium text-light-content dark:text-dark-content';
 	$: finalClass = twMerge(defaultClass, $$props.class);
 </script>
 
-<h3 class={finalClass} style={$$props.style}>
+<h3
+	class={finalClass}
+	use:useActions={use}
+	use:forwardEvents
+	{...exclude($$props, ['use', 'class'])}
+>
 	<slot />
 </h3>
