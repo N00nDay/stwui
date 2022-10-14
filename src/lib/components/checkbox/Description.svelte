5843857000aba4e8e8c14f8afa4f46fd16a01281
Craig Howell <howell.craig@gmail.com>
2022-10-14 12:15:13 -0400

    fix(CheckboxGroup.Checkbox.Description): forwarding all events fix(CheckboxGroup.Label): forwarding all events

--- src/lib/components/checkbox/Description.svelte
+++ src/lib/components/checkbox/Description.svelte
@@ -4,6 +4,11 @@
 	import { CHECKBOX_GROUP_CHECKBOX_CONTEXT_ID } from './Checkbox.svelte';
 	import { useContext } from '../../utils/useContext';
 	import { getContext } from 'svelte';
+	import { get_current_component } from 'svelte/internal';
+	import { forwardEventsBuilder, useActions, type ActionArray } from '../../actions';
+	export let use: ActionArray = [];
+	import { exclude } from '../../utils/exclude';
+	const forwardEvents = forwardEventsBuilder(get_current_component());
 
 	useContext({
 		context_id: CHECKBOX_GROUP_CONTEXT_ID,
@@ -25,9 +30,23 @@
 </script>
 
 {#if inline}
-	<span id="{name}-description" class={finalClass} style={$$props.style}> | <slot /></span>
+	<span
+		id="{name}-description"
+		class={finalClass}
+		use:useActions={use}
+		use:forwardEvents
+		{...exclude($$props, ['use', 'class'])}
+	>
+		| <slot /></span
+	>
 {:else}
-	<p id="{name}-description" class={finalClass} style={$$props.style}>
+	<p
+		id="{name}-description"
+		class={finalClass}
+		use:useActions={use}
+		use:forwardEvents
+		{...exclude($$props, ['use', 'class'])}
+	>
 		<slot />
 	</p>
 {/if}
--- src/lib/components/checkbox/GroupLabel.svelte
+++ src/lib/components/checkbox/GroupLabel.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import { twMerge } from 'tailwind-merge';
+	import { get_current_component } from 'svelte/internal';
+	import { forwardEventsBuilder, useActions, type ActionArray } from '../../actions';
+	export let use: ActionArray = [];
+	import { exclude } from '../../utils/exclude';
+	const forwardEvents = forwardEventsBuilder(get_current_component());
 
 	const defaultClass =
 		'text-light-secondary-content dark:text-dark-secondary-content font-medium text-sm';
 	$: finalClass = twMerge(defaultClass, $$props.class);
 </script>
 
-<legend class={finalClass} style={$$props.style}><slot /></legend>
+<legend
+	class={finalClass}
+	use:useActions={use}
+	use:forwardEvents
+	{...exclude($$props, ['use', 'class'])}><slot /></legend
+>
