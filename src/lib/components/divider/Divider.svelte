7c4782b5513261b98c90c95dfaeedeffca7cb7bd
Craig Howell <howell.craig@gmail.com>
2022-10-14 12:37:41 -0400

    fix(Divider): forward all events including  child components

--- src/lib/components/divider/Divider.svelte
+++ src/lib/components/divider/Divider.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import { twMerge } from 'tailwind-merge';
+	import { get_current_component } from 'svelte/internal';
+	import { forwardEventsBuilder, useActions, type ActionArray } from '../../actions';
+	export let use: ActionArray = [];
+	import { exclude } from '../../utils/exclude';
+	const forwardEvents = forwardEventsBuilder(get_current_component());
 
 	export let position: 'left' | 'center' | 'right' = 'center';
 
@@ -9,7 +14,12 @@
 
 <div class="relative">
 	<div class="absolute inset-0 flex items-center" aria-hidden="true">
-		<div class={finalClass} style={$$props.style} />
+		<div
+			class={finalClass}
+			use:useActions={use}
+			use:forwardEvents
+			{...exclude($$props, ['use', 'class'])}
+		/>
 	</div>
 	{#if $$slots.label || $$slots.icon || $$slots.default}
 		<div
--- src/lib/components/divider/Icon.svelte
+++ src/lib/components/divider/Icon.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import type { MaterialIcon } from '../../types';
 	import { twMerge } from 'tailwind-merge';
+	import { get_current_component } from 'svelte/internal';
+	import { forwardEventsBuilder, useActions, type ActionArray } from '../../actions';
+	export let use: ActionArray = [];
+	import { exclude } from '../../utils/exclude';
+	const forwardEvents = forwardEventsBuilder(get_current_component());
 
 	export let icon: MaterialIcon;
 
@@ -9,4 +14,9 @@
 	$: finalClass = twMerge(defaultClass, $$props.class);
 </script>
 
-<span class={finalClass} style={$$props.style}>{icon}</span>
+<span
+	class={finalClass}
+	use:useActions={use}
+	use:forwardEvents
+	{...exclude($$props, ['use', 'class'])}>{icon}</span
+>
