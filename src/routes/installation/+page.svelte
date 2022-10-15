3da1cb49dce20ab363fe1d0962318686ddc8b381
Craig Howell <howell.craig@gmail.com>
2022-10-14 20:55:41 -0400

    docs(installation): added install and setup information

--- src/routes/installation/+page.svelte
+++ src/routes/installation/+page.svelte
@@ -2,7 +2,18 @@
 	import { Col, Divider } from '$lib';
 	import { CodeBlock, InlineCode } from '../../docs';
 
+	const svelteKitSetUp = `
+npm create svelte@latest my-app
+cd my-app
+npm install
+`;
+
 	const installCode = 'npm i -D stwui';
+
+	const tailwindInstall = `
+npx svelte-add@latest tailwindcss
+npm install
+`;
 
 	const tailwindCode = `
 module.exports = {
@@ -24,7 +35,19 @@ module.exports = {
 
 <Col class="col-24">
 	<div class="w-full h-full overflow-auto">
-		<h1 class="text-light-content dark:text-dark-content mb-0">Installation</h1>
+		<h1 class="text-light-content dark:text-dark-content mb-0">Create SvelteKit project</h1>
+		<Divider />
+
+		<p class="mb-3 mt-5 text-light-content dark:text-dark-content">
+			Create a svelte-kit project with the commands below. Visit <a
+				href="https://kit.svelte.dev/docs/introduction#getting-started"
+				class="link">kit.svelte</a
+			> to view the official documentation:
+		</p>
+
+		<CodeBlock language="bash" code={svelteKitSetUp} />
+
+		<h1 class="text-light-content dark:text-dark-content mb-0 mt-12">Install STWUI</h1>
 		<Divider />
 
 		<p class="mb-3 mt-5 text-light-content dark:text-dark-content">
@@ -32,6 +55,15 @@ module.exports = {
 		</p>
 
 		<CodeBlock language="bash" code={installCode} />
+
+		<h1 class="text-light-content dark:text-dark-content mb-0 mt-12">Install TailwindCSS</h1>
+		<Divider />
+
+		<p class="mb-3 mt-5 text-light-content dark:text-dark-content">
+			Run the following command to add STWUI to your project:
+		</p>
+
+		<CodeBlock language="bash" code={tailwindInstall} />
 
 		<h1 class="text-light-content dark:text-dark-content mb-0 mt-12">Configure TailwindCSS</h1>
 		<Divider />
