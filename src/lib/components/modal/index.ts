dbae9efbd4f433dd13b839ad213a277489041e01
Craig Howell <howell.craig@gmail.com>
2023-03-08 15:11:50 -0500

    feat(Modal): added new syntax, header, footer, body, and long content scrolling

--- src/lib/components/modal/index.ts
+++ src/lib/components/modal/index.ts
@@ -1,15 +1,28 @@
 import OriginalModal from './Modal.svelte';
 import Backdrop from './Backdrop.svelte';
-import Content from './Content.svelte';
+import OriginalContent from './Content.svelte';
+import Header from './Header.svelte';
+import Body from './Body.svelte';
+import Footer from './Footer.svelte';
 
 const Modal = OriginalModal as ModalStatic;
 Modal.Backdrop = Backdrop;
-Modal.Content = Content;
+Modal.Content = OriginalContent as ContentStatic;
+Modal.Content.Header = Header;
+Modal.Content.Body = Body;
+Modal.Content.Footer = Footer;
 
 export default Modal;
 
 export interface ModalStatic {
 	new (...args: ConstructorParameters<typeof OriginalModal>): OriginalModal;
 	Backdrop: typeof Backdrop;
-	Content: typeof Content;
+	Content: ContentStatic;
+}
+
+export interface ContentStatic {
+	new (...args: ConstructorParameters<typeof OriginalContent>): OriginalContent;
+	Header: typeof Header;
+	Body: typeof Body;
+	Footer: typeof Footer;
 }
