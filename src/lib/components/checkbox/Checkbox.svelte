ee73e988bdd2c6580875aca81213dc0eaf76bfcb
Kjartan Emil Dige <kd@aviou.io>
2023-03-12 21:52:04 +0100

    fix(chekbox): add disabled and checked props

--- src/lib/components/checkbox/Checkbox.svelte
+++ src/lib/components/checkbox/Checkbox.svelte
@@ -5,15 +5,21 @@
 	export let name: string;
 	export let value: string;
 	export let indeterminate = false;
+	export let checked = false;
+	export let disabled = false;
 
 	const defaultClass =
-		'checkbox checked:bg-primary dark:checked:bg-primary indeterminate:bg-primary dark:indeterminate:bg-primary hover:border-primary dark:hover:border-primary bg-light-surface dark:bg-dark-surface h-6 w-6 text-primary border-light-border-base dark:border-dark-border-base rounded-md cursor-pointer';
+		'checkbox checked:bg-primary dark:checked:bg-primary indeterminate:bg-primary dark:indeterminate:bg-primary hover:border-primary dark:hover:border-primary bg-light-surface dark:bg-dark-surface h-6 w-6 text-primary border-light-border-base dark:border-dark-border-base rounded-md cursor-pointer disabled:checked:bg-gray-300 dark:disabled:checked:bg-zinc-600';
 	$: finalClass = twMerge(defaultClass, $$props.class);
 
 	setContext('checkbox-name', name);
 </script>
 
-<div class="relative flex items-start">
+<div
+	class="relative flex items-start"
+	class:pointer-events-none={disabled}
+	class:opacity-50={disabled}
+>
 	<div class="flex items-center h-5">
 		<input
 			id={name}
@@ -23,6 +29,8 @@
 			{value}
 			class={finalClass}
 			style={$$props.style}
+			{checked}
+			{disabled}
 		/>
 	</div>
 	<div class="ml-3 text-sm">
