b2b7fd4c98ba6fc9dde6ec9097f58042b08f3ccf
Craig Howell <howell.craig@gmail.com>
2023-03-15 21:32:36 -0400

    docs(Accordion): updated with new ExampleContainer

--- src/routes/accordion/+page.svelte
+++ src/routes/accordion/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Accordion, Card, Col } from '../../lib';
 	import { example, slots, itemSlots, itemProps, titleSlots, contentSlots } from './examples';
-	import { PropsTable, SlotsTable, CodeBlock } from '../../docs';
+	import { PropsTable, SlotsTable, CodeBlock, ExampleContainer } from '../../docs';
 
 	let open = '';
 
@@ -14,65 +14,65 @@
 	}
 </script>
 
-<Col class="col-24 md:col-12">
-	<Card bordered={false}>
-		<Card.Content slot="content" class="p-4">
-			<Accordion>
-				<Accordion.Item open={open === 'open1'}>
-					<Accordion.Item.Title slot="title" on:click={() => handleClick('open1')}
-						>Item 1</Accordion.Item.Title
-					>
-					<Accordion.Item.Content slot="content">
-						<div class="px-4 py-5">
-							<strong>This is the first item's accordion body.</strong> It is shown by default,
-							until the collapse plugin adds the appropriate classes that we use to style each
-							element. These classes control the overall appearance, as well as the showing and
-							hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-							our default variables. It's also worth noting that just about any HTML can go within
-							the
-							<code>.accordion-body</code>, though the transition does limit overflow.
-						</div>
-					</Accordion.Item.Content>
-				</Accordion.Item>
-				<Accordion.Item open={open === 'open2'}>
-					<Accordion.Item.Title slot="title" on:click={() => handleClick('open2')}
-						>Item 2</Accordion.Item.Title
-					>
-					<Accordion.Item.Content slot="content">
-						<div class="px-4 py-5">
-							<strong>This is the first item's accordion body.</strong> It is shown by default,
-							until the collapse plugin adds the appropriate classes that we use to style each
-							element. These classes control the overall appearance, as well as the showing and
-							hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-							our default variables. It's also worth noting that just about any HTML can go within
-							the
-							<code>.accordion-body</code>, though the transition does limit overflow.
-						</div>
-					</Accordion.Item.Content>
-				</Accordion.Item>
-				<Accordion.Item open={open === 'open3'}>
-					<Accordion.Item.Title slot="title" on:click={() => handleClick('open3')}
-						>Item 3</Accordion.Item.Title
-					>
-					<Accordion.Item.Content slot="content">
-						<div class="px-4 py-5">
-							<strong>This is the first item's accordion body.</strong> It is shown by default,
-							until the collapse plugin adds the appropriate classes that we use to style each
-							element. These classes control the overall appearance, as well as the showing and
-							hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-							our default variables. It's also worth noting that just about any HTML can go within
-							the
-							<code>.accordion-body</code>, though the transition does limit overflow.
-						</div>
-					</Accordion.Item.Content>
-				</Accordion.Item>
-			</Accordion>
+<Col class="col-24 example-container">
+	<ExampleContainer title="Basic Accordion">
+		<Card slot="preview" bordered={false}>
+			<Card.Content slot="content" class="p-4">
+				<Accordion class="m-auto max-w-[400px]">
+					<Accordion.Item open={open === 'open1'}>
+						<Accordion.Item.Title slot="title" on:click={() => handleClick('open1')}
+							>Item 1</Accordion.Item.Title
+						>
+						<Accordion.Item.Content slot="content">
+							<div class="px-4 py-5">
+								<strong>This is the first item's accordion body.</strong> It is shown by default,
+								until the collapse plugin adds the appropriate classes that we use to style each
+								element. These classes control the overall appearance, as well as the showing and
+								hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+								our default variables. It's also worth noting that just about any HTML can go within
+								the
+								<code>.accordion-body</code>, though the transition does limit overflow.
+							</div>
+						</Accordion.Item.Content>
+					</Accordion.Item>
+					<Accordion.Item open={open === 'open2'}>
+						<Accordion.Item.Title slot="title" on:click={() => handleClick('open2')}
+							>Item 2</Accordion.Item.Title
+						>
+						<Accordion.Item.Content slot="content">
+							<div class="px-4 py-5">
+								<strong>This is the first item's accordion body.</strong> It is shown by default,
+								until the collapse plugin adds the appropriate classes that we use to style each
+								element. These classes control the overall appearance, as well as the showing and
+								hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+								our default variables. It's also worth noting that just about any HTML can go within
+								the
+								<code>.accordion-body</code>, though the transition does limit overflow.
+							</div>
+						</Accordion.Item.Content>
+					</Accordion.Item>
+					<Accordion.Item open={open === 'open3'}>
+						<Accordion.Item.Title slot="title" on:click={() => handleClick('open3')}
+							>Item 3</Accordion.Item.Title
+						>
+						<Accordion.Item.Content slot="content">
+							<div class="px-4 py-5">
+								<strong>This is the first item's accordion body.</strong> It is shown by default,
+								until the collapse plugin adds the appropriate classes that we use to style each
+								element. These classes control the overall appearance, as well as the showing and
+								hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+								our default variables. It's also worth noting that just about any HTML can go within
+								the
+								<code>.accordion-body</code>, though the transition does limit overflow.
+							</div>
+						</Accordion.Item.Content>
+					</Accordion.Item>
+				</Accordion>
+			</Card.Content>
+		</Card>
 
-			<br />
-
-			<CodeBlock language="svelte" code={example} />
-		</Card.Content>
-	</Card>
+		<CodeBlock slot="code" language="svelte" code={example} />
+	</ExampleContainer>
 </Col>
 
 <Col class="col-24">
