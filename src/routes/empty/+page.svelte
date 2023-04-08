aa9d11f16ab6ff565d439d42495183369eb33c86
Craig Howell <howell.craig@gmail.com>
2023-04-08 13:10:03 -0400

    docs(Empty): updated examples and layouts

--- src/routes/empty/+page.svelte
+++ src/routes/empty/+page.svelte
@@ -1,51 +1,37 @@
 <script lang="ts">
-	import { Button, Card, Col, Empty } from '../../lib';
+	import { Button, Empty } from '../../lib';
 	import { example, slots, iconProps, titleSlots, descriptionSlots, actionSlots } from './examples';
-	import { PropsTable, SlotsTable, CodeBlock } from '../../docs';
+	import { PropsTable, SlotsTable, CodeBlock, ExampleContainer } from '../../docs';
 
 	import { plus_circle, lock } from '../../docs/icons';
 </script>
 
-<Col class="col-24 md:col-12">
-	<Card bordered={false}>
-		<Card.Content slot="content" class="p-4">
-			<Empty>
-				<Empty.Icon slot="icon" data={lock} />
-				<Empty.Title slot="title">No Projects</Empty.Title>
-				<Empty.Description slot="description">
-					Get started by creating a new project.
-				</Empty.Description>
-				<Empty.Action slot="action">
-					<Button type="primary" on:click={() => console.log('clicked new project')}>
-						<Button.Leading slot="leading" data={plus_circle} />
-						New Project
-					</Button>
-				</Empty.Action>
-			</Empty>
+<ExampleContainer title="Avatar Dropdown">
+	<div slot="preview" class="w-full flex gap-2 justify-center items-center">
+		<Empty>
+			<Empty.Icon slot="icon" data={lock} />
+			<Empty.Title slot="title">No Projects</Empty.Title>
+			<Empty.Description slot="description">
+				Get started by creating a new project.
+			</Empty.Description>
+			<Empty.Action slot="action">
+				<Button type="primary" on:click={() => console.log('clicked new project')}>
+					<Button.Leading slot="leading" data={plus_circle} />
+					New Project
+				</Button>
+			</Empty.Action>
+		</Empty>
+	</div>
 
-			<br />
+	<CodeBlock slot="code" language="svelte" code={example} />
+</ExampleContainer>
 
-			<CodeBlock language="svelte" code={example} />
-		</Card.Content>
-	</Card>
-</Col>
+<SlotsTable component="Empty" {slots} />
 
-<Col class="col-24">
-	<SlotsTable component="Empty" {slots} />
-</Col>
+<PropsTable component="Empty.Icon" props={iconProps} />
 
-<Col class="col-24">
-	<PropsTable component="Empty.Icon" props={iconProps} />
-</Col>
+<SlotsTable component="Empty.Title" slots={titleSlots} />
 
-<Col class="col-24">
-	<SlotsTable component="Empty.Title" slots={titleSlots} />
-</Col>
+<SlotsTable component="Empty.Description" slots={descriptionSlots} />
 
-<Col class="col-24">
-	<SlotsTable component="Empty.Description" slots={descriptionSlots} />
-</Col>
-
-<Col class="col-24">
-	<SlotsTable component="Empty.Action" slots={actionSlots} />
-</Col>
+<SlotsTable component="Empty.Action" slots={actionSlots} />
