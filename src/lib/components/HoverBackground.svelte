6bce06ef76fe27c3e1db78ed9a6a584565d3ff0d
Craig Howell <howell.craig@gmail.com>
2023-04-24 19:36:24 -0400

    fix(HoverBackground): added group-focus instead of relying on just group-hover

--- src/lib/components/HoverBackground.svelte
+++ src/lib/components/HoverBackground.svelte
@@ -2,7 +2,7 @@
 	import { twMerge } from 'tailwind-merge';
 
 	const defaultClass =
-		'pointer-events-none absolute bg-hover	 duration-150 transition-opacity h-full w-full inset-0 group-hover:opacity-[0.07] opacity-0';
+		'pointer-events-none absolute bg-hover	duration-150 transition-opacity h-full w-full inset-0 group-hover:opacity-[0.07] group-focus:opacity-[0.07] opacity-0';
 	$: finalClass = twMerge(defaultClass, $$props.class);
 </script>
 
