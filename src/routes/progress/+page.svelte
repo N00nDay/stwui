cd8039e1e7fda5c11bbf761310e412600273379e
Craig Howell <howell.craig@gmail.com>
2023-06-14 19:45:08 -0400

    docs(Progress): updated examples and layout

--- src/routes/progress/+page.svelte
+++ src/routes/progress/+page.svelte
@@ -1,81 +1,61 @@
 <script lang="ts">
-	import { Card, Col, Progress } from '../../lib';
+	import { Progress } from '../../lib';
 	import { example, props, radialExample, typeExample } from './examples';
-	import { PropsTable, CodeBlock } from '../../docs';
+	import { PropsTable, CodeBlock, ExampleContainer } from '../../docs';
 </script>
 
-<Col class="col-24 md:col-12">
-	<Card bordered={false}>
-		<Card.Header slot="header">Default</Card.Header>
-		<Card.Content slot="content" class="p-4">
-			<Progress value={25} />
-			<br />
-			<Progress value={50} />
-			<br />
-			<Progress value={75} displayValue />
-			<br />
-			<Progress value={100} />
-			<br />
-			<Progress value={0} indeterminate />
+<ExampleContainer title="Basic">
+	<div slot="preview" class="w-full max-w-xl block m-auto">
+		<Progress value={25} />
+		<br />
+		<Progress value={50} />
+		<br />
+		<Progress value={75} displayValue />
+		<br />
+		<Progress value={100} />
+		<br />
+		<Progress value={0} indeterminate />
+	</div>
+	<CodeBlock slot="code" language="svelte" code={example} />
+</ExampleContainer>
 
-			<br />
+<ExampleContainer title="Radial and Size">
+	<div slot="preview" class="w-full max-w-xl block m-auto">
+		<Progress value={25} size="xs" />
+		<br />
+		<Progress value={50} size="sm" />
+		<br />
+		<Progress value={75} />
+		<br />
+		<Progress value={85} size="lg" />
+		<br />
+		<Progress value={100} size="xl" />
+		<br />
+		<Progress value={25} radial size="xs" displayValue />
+		<Progress value={50} radial size="sm" />
+		<Progress value={75} radial />
+		<Progress value={85} radial size="lg" displayValue />
+		<Progress value={100} radial size="xl" />
+	</div>
+	<CodeBlock slot="code" language="svelte" code={radialExample} />
+</ExampleContainer>
 
-			<CodeBlock language="svelte" code={example} />
-		</Card.Content>
-	</Card>
-</Col>
+<ExampleContainer title="Progress with Type">
+	<div slot="preview" class="w-full max-w-xl block m-auto">
+		<Progress value={75} type="error" />
+		<br />
+		<Progress value={75} type="success" />
+		<br />
+		<Progress value={75} type="warn" />
+		<br />
+		<Progress value={75} />
+		<br />
+		<Progress value={75} radial type="error" displayValue />
+		<Progress value={75} radial type="success" displayValue />
+		<Progress value={75} radial type="warn" displayValue />
+		<Progress value={75} radial displayValue />
+	</div>
+	<CodeBlock slot="code" language="svelte" code={typeExample} />
+</ExampleContainer>
 
-<Col class="col-24 md:col-12">
-	<Card bordered={false}>
-		<Card.Header slot="header">Radial and Size</Card.Header>
-		<Card.Content slot="content" class="p-4">
-			<Progress value={25} size="xs" />
-			<br />
-			<Progress value={50} size="sm" />
-			<br />
-			<Progress value={75} />
-			<br />
-			<Progress value={85} size="lg" />
-			<br />
-			<Progress value={100} size="xl" />
-			<br />
-			<Progress value={25} radial size="xs" displayValue />
-			<Progress value={50} radial size="sm" />
-			<Progress value={75} radial />
-			<Progress value={85} radial size="lg" displayValue />
-			<Progress value={100} radial size="xl" />
-
-			<br />
-
-			<CodeBlock language="svelte" code={radialExample} />
-		</Card.Content>
-	</Card>
-</Col>
-
-<Col class="col-24 md:col-12">
-	<Card bordered={false}>
-		<Card.Header slot="header">Progress with Type</Card.Header>
-		<Card.Content slot="content" class="p-4">
-			<Progress value={75} type="error" />
-			<br />
-			<Progress value={75} type="success" />
-			<br />
-			<Progress value={75} type="warn" />
-			<br />
-			<Progress value={75} />
-			<br />
-			<Progress value={75} radial type="error" displayValue />
-			<Progress value={75} radial type="success" displayValue />
-			<Progress value={75} radial type="warn" displayValue />
-			<Progress value={75} radial displayValue />
-
-			<br />
-
-			<CodeBlock language="svelte" code={typeExample} />
-		</Card.Content>
-	</Card>
-</Col>
-
-<Col class="col-24">
-	<PropsTable component="Progress" {props} />
-</Col>
+<PropsTable component="Progress" {props} />
--- src/routes/progress/examples.ts
+++ src/routes/progress/examples.ts
@@ -45,13 +45,13 @@ export const example = `
 </script>
 
 <Progress value={25} />
-<br />
+
 <Progress value={50} />
-<br />
+
 <Progress value={75} displayValue />
-<br />
+
 <Progress value={100} />
-<br />
+
 <Progress value={0} indeterminate />`;
 
 export const radialExample = `
@@ -60,19 +60,23 @@ export const radialExample = `
 </script>
 
 <Progress value={25} size="xs" />
-<br />
+
 <Progress value={50} size="sm" />
-<br />
+
 <Progress value={75} />
-<br />
+
 <Progress value={85} size="lg" />
-<br />
+
 <Progress value={100} size="xl" />
-<br />
+
 <Progress value={25} radial size="xs" />
+
 <Progress value={50} radial size="sm" />
+
 <Progress value={75} radial />
+
 <Progress value={85} radial size="lg" />
+
 <Progress value={100} radial size="xl" />`;
 
 export const typeExample = `
@@ -81,14 +85,17 @@ export const typeExample = `
 </script>
 
 <Progress value={75} type="error" />
-<br />
+
 <Progress value={75} type="success" />
-<br />
+
 <Progress value={75} type="warn" />
-<br />
+
 <Progress value={75} />
-<br />
+
 <Progress value={75} radial type="error" />
+
 <Progress value={75} radial type="success" />
+
 <Progress value={75} radial type="warn" />
+
 <Progress value={75} radial />`;
