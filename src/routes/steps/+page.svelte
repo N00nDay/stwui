603690dfe6e56516ecfa820ebe1efa64f1dfe481
Craig Howell <howell.craig@gmail.com>
2023-06-16 19:49:11 -0400

    docs(Steps): updated layout

--- src/routes/steps/+page.svelte
+++ src/routes/steps/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Card, Col, Steps } from '../../lib';
+	import { Steps } from '../../lib';
 	import {
 		example1,
 		example2,
@@ -12,7 +12,7 @@
 		titleSlots,
 		descriptionSlots
 	} from './examples';
-	import { PropsTable, SlotsTable, CodeBlock } from '../../docs';
+	import { PropsTable, SlotsTable, CodeBlock, ExampleContainer } from '../../docs';
 
 	let currentStep = 2;
 
@@ -35,105 +35,69 @@
 	];
 </script>
 
-<Col class="col-24">
-	<Card bordered={false}>
-		<Card.Header slot="header">Simple</Card.Header>
-		<Card.Content slot="content" class="p-4">
-			<Steps {currentStep}>
-				{#each steps as step, i}
-					<Steps.Step step={i + 1} href={step.href} on:click={() => (currentStep = i + 1)}>
-						<Steps.Step.Title slot="title">{step.title}</Steps.Step.Title>
-						<Steps.Step.Description slot="description">{step.description}</Steps.Step.Description>
-					</Steps.Step>
-				{/each}
-			</Steps>
+<ExampleContainer title="Simple">
+	<div slot="preview" class="w-full">
+		<Steps {currentStep}>
+			{#each steps as step, i}
+				<Steps.Step step={i + 1} href={step.href} on:click={() => (currentStep = i + 1)}>
+					<Steps.Step.Title slot="title">{step.title}</Steps.Step.Title>
+					<Steps.Step.Description slot="description">{step.description}</Steps.Step.Description>
+				</Steps.Step>
+			{/each}
+		</Steps>
+	</div>
+	<CodeBlock slot="code" language="svelte" code={example1} />
+</ExampleContainer>
 
-			<br />
+<ExampleContainer title="Bullets">
+	<div slot="preview" class="w-full">
+		<Steps variant="bullets" {currentStep}>
+			<Steps.Summary slot="summary">Step {currentStep} of {steps.length}</Steps.Summary>
+			{#each steps as step, i}
+				<Steps.Step step={i + 1} href={step.href} on:click={() => (currentStep = i + 1)}>
+					<Steps.Step.Title slot="title">{step.title}</Steps.Step.Title>
+				</Steps.Step>
+			{/each}
+		</Steps>
+	</div>
+	<CodeBlock slot="code" language="svelte" code={example2} />
+</ExampleContainer>
 
-			<CodeBlock language="svelte" code={example1} />
-		</Card.Content>
-	</Card>
-</Col>
+<ExampleContainer title="Bullets & Text">
+	<div slot="preview" class="w-full">
+		<Steps variant="bullets-text" {currentStep}>
+			{#each steps as step, i}
+				<Steps.Step step={i + 1} href={step.href} on:click={() => (currentStep = i + 1)}>
+					<Steps.Step.Title slot="title">{step.title}</Steps.Step.Title>
+				</Steps.Step>
+			{/each}
+		</Steps>
+	</div>
+	<CodeBlock slot="code" language="svelte" code={example3} />
+</ExampleContainer>
 
-<Col class="col-24">
-	<Card bordered={false}>
-		<Card.Header slot="header">Bullets</Card.Header>
-		<Card.Content slot="content" class="p-4">
-			<Steps variant="bullets" {currentStep}>
-				<Steps.Summary slot="summary">Step {currentStep} of {steps.length}</Steps.Summary>
-				{#each steps as step, i}
-					<Steps.Step step={i + 1} href={step.href} on:click={() => (currentStep = i + 1)}>
-						<Steps.Step.Title slot="title">{step.title}</Steps.Step.Title>
-					</Steps.Step>
-				{/each}
-			</Steps>
+<ExampleContainer title="Circles & Text">
+	<div slot="preview" class="w-full">
+		<Steps variant="circles-text" {currentStep}>
+			{#each steps as step, i}
+				<Steps.Step step={i + 1} href={step.href} on:click={() => (currentStep = i + 1)}>
+					<Steps.Step.Title slot="title">{step.title}</Steps.Step.Title>
+					<Steps.Step.Description slot="description">{step.description}</Steps.Step.Description>
+				</Steps.Step>
+			{/each}
+		</Steps>
+	</div>
+	<CodeBlock slot="code" language="svelte" code={example4} />
+</ExampleContainer>
 
-			<br />
+<PropsTable component="Steps" {props} />
 
-			<CodeBlock language="svelte" code={example2} />
-		</Card.Content>
-	</Card>
-</Col>
+<SlotsTable component="Steps" {slots} />
 
-<Col class="col-24">
-	<Card bordered={false}>
-		<Card.Header slot="header">Bullets & Text</Card.Header>
-		<Card.Content slot="content" class="p-4">
-			<Steps variant="bullets-text" {currentStep}>
-				{#each steps as step, i}
-					<Steps.Step step={i + 1} href={step.href} on:click={() => (currentStep = i + 1)}>
-						<Steps.Step.Title slot="title">{step.title}</Steps.Step.Title>
-					</Steps.Step>
-				{/each}
-			</Steps>
+<PropsTable component="Steps.Step" props={stepProps} />
 
-			<br />
+<SlotsTable component="Steps.Step" slots={stepSlots} />
 
-			<CodeBlock language="svelte" code={example3} />
-		</Card.Content>
-	</Card>
-</Col>
+<SlotsTable component="Steps.Step.Title" slots={titleSlots} />
 
-<Col class="col-24">
-	<Card bordered={false}>
-		<Card.Header slot="header">Circles & Text</Card.Header>
-		<Card.Content slot="content" class="p-4">
-			<Steps variant="circles-text" {currentStep}>
-				{#each steps as step, i}
-					<Steps.Step step={i + 1} href={step.href} on:click={() => (currentStep = i + 1)}>
-						<Steps.Step.Title slot="title">{step.title}</Steps.Step.Title>
-						<Steps.Step.Description slot="description">{step.description}</Steps.Step.Description>
-					</Steps.Step>
-				{/each}
-			</Steps>
-
-			<br />
-
-			<CodeBlock language="svelte" code={example4} />
-		</Card.Content>
-	</Card>
-</Col>
-
-<Col class="col-24">
-	<PropsTable component="Steps" {props} />
-</Col>
-
-<Col class="col-24">
-	<SlotsTable component="Steps" {slots} />
-</Col>
-
-<Col class="col-24">
-	<PropsTable component="Steps.Step" props={stepProps} />
-</Col>
-
-<Col class="col-24">
-	<SlotsTable component="Steps.Step" slots={stepSlots} />
-</Col>
-
-<Col class="col-24">
-	<SlotsTable component="Steps.Step.Title" slots={titleSlots} />
-</Col>
-
-<Col class="col-24">
-	<SlotsTable component="Steps.Step.Description" slots={descriptionSlots} />
-</Col>
+<SlotsTable component="Steps.Step.Description" slots={descriptionSlots} />
