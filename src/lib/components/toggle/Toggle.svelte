e2387e193ba93a27f5e1253cb5081ec545e74062
Craig Howell <howell.craig@gmail.com>
2023-06-16 20:52:55 -0400

    fix(Toggle): fixed error placement in flex container

--- src/lib/components/toggle/Toggle.svelte
+++ src/lib/components/toggle/Toggle.svelte
@@ -31,38 +31,40 @@
 	setContext('toggle-off', toggleOff);
 </script>
 
-<div class="flex items-center">
-	<slot name="content-left" />
-	<div
-		class="border border-border relative inline-flex flex-shrink-0 h-[1.6rem] rounded-full cursor-pointer outline-none focus:outline-none {width}"
-	>
-		<button
-			aria-label="toggle"
-			on:click={toggle}
-			type="button"
-			class="relative inline-flex items-center justify-between flex-shrink-0 h-full w-full border-2 border-transparent rounded-full cursor-pointer ease-in-out outline-none focus:outline-none"
-			class:bg-default={!on}
-			class:bg-primary={on}
+<div class="flex flex-col">
+	<div class="flex items-center">
+		<slot name="content-left" />
+		<div
+			class="border border-border relative inline-flex flex-shrink-0 h-[1.6rem] rounded-full cursor-pointer outline-none focus:outline-none {width}"
 		>
-			<span
-				aria-hidden="true"
-				class="relative z-10 inline-block bg-surface h-5 w-5 rounded-full shadow transform transition-transform ease-in-out duration-150 border border-border {translate}"
-			/>
-			<input type="checkbox" class="hidden" {name} id={name} checked={on} />
-			{#if $$slots['left-icon']}
-				<span class="text-sm absolute left-0.5 flex items-center justify-center h-4 w-4">
-					<slot name="left-icon" />
-				</span>
-			{/if}
-			{#if $$slots['right-icon']}
-				<span class="text-sm absolute right-0.5 flex items-center justify-center h-4 w-4">
-					<slot name="right-icon" />
-				</span>
-			{/if}
-		</button>
+			<button
+				aria-label="toggle"
+				on:click={toggle}
+				type="button"
+				class="relative inline-flex items-center justify-between flex-shrink-0 h-full w-full border-2 border-transparent rounded-full cursor-pointer ease-in-out outline-none focus:outline-none"
+				class:bg-default={!on}
+				class:bg-primary={on}
+			>
+				<span
+					aria-hidden="true"
+					class="relative z-10 inline-block bg-surface h-5 w-5 rounded-full shadow transform transition-transform ease-in-out duration-150 border border-border {translate}"
+				/>
+				<input type="checkbox" class="hidden" {name} id={name} checked={on} />
+				{#if $$slots['left-icon']}
+					<span class="text-sm absolute left-0.5 flex items-center justify-center h-4 w-4">
+						<slot name="left-icon" />
+					</span>
+				{/if}
+				{#if $$slots['right-icon']}
+					<span class="text-sm absolute right-0.5 flex items-center justify-center h-4 w-4">
+						<slot name="right-icon" />
+					</span>
+				{/if}
+			</button>
+		</div>
+		<slot name="content-right" />
 	</div>
-	<slot name="content-right" />
+	{#if error}
+		<p transition:slide|local class="!mt-2 text-sm text-danger" id="{name}-error">{error}</p>
+	{/if}
 </div>
-{#if error}
-	<p transition:slide|local class="!mt-2 text-sm text-danger" id="{name}-error">{error}</p>
-{/if}
