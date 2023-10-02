9ea9f3161ff7e460abdb1d420be6e44b7583f5a5
Craig Howell <howell.craig@gmail.com>
2023-10-01 21:09:12 -0400

    docs: added ClassTable.svelte

---
+++ src/docs/components/class-table/ClassTable.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { TableColumn } from '../../../lib/types/table-column';
+	import { Card, Table, Col } from '../../../lib';
+	import type { Prop } from '../../types/props-table';
+
+	export let component: string;
+	export let props: Prop[];
+
+	const columns: TableColumn[] = [
+		{
+			column: 'class',
+			label: 'Class',
+			placement: 'left'
+		}
+	];
+</script>
+
+<Col class="col-24">
+	<h1 class="mb-4 mt-8 text-xl font-semibold text-content">
+		{component} Class Identifiers
+	</h1>
+	<Card bordered={false}>
+		<Table class="rounded-md overflow-hidden h-full" {columns}>
+			<Table.Header slot="header" sortable={false} orderBy="id" />
+			<Table.Body slot="body">
+				{#each props as prop}
+					<Table.Body.Row id={prop.id} class="cursor-default hover:bg-surface">
+						<Table.Body.Row.Cell column={0}>{prop.prop}</Table.Body.Row.Cell>
+					</Table.Body.Row>
+				{/each}
+			</Table.Body>
+		</Table>
+	</Card>
+</Col>
