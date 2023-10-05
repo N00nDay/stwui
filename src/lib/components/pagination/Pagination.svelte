8508d3b0ffd643c5839659de6e8622da3ce37d1a
Craig Howell <howell.craig@gmail.com>
2023-10-04 20:43:44 -0400

    fix(Pagination): added class identifiers

--- src/lib/components/pagination/Pagination.svelte
+++ src/lib/components/pagination/Pagination.svelte
@@ -36,13 +36,13 @@
 	$: pagination = pageRange(currentPage, totalPages);
 </script>
 
-<div class="flex items-center justify-between px-4 py-3 sm:px-6">
+<div class="flex items-center justify-between px-4 py-3 sm:px-6 stwui-pagination">
 	<div class="flex flex-1 justify-between md:hidden">
 		<button
 			type="button"
 			on:click={onPreviousClick}
 			disabled={currentPage === 1}
-			class="relative group shadow-sm overflow-hidden inline-flex items-center rounded-md border border-border bg-surface px-4 py-2 text-sm font-medium text-secondary-content"
+			class="stwui-pagination-previous relative group shadow-sm overflow-hidden inline-flex items-center rounded-md border border-border bg-surface px-4 py-2 text-sm font-medium text-secondary-content"
 			class:pointer-events-none={currentPage === 1}
 			>Previous
 			<HoverBackground />
@@ -51,13 +51,13 @@
 			type="button"
 			on:click={onNextClick}
 			disabled={end === total}
-			class="relative group shadow-sm overflow-hidden inline-flex items-center rounded-md border border-border bg-surface px-4 py-2 text-sm font-medium text-secondary-content"
+			class="stwui-pagination-next relative group shadow-sm overflow-hidden inline-flex items-center rounded-md border border-border bg-surface px-4 py-2 text-sm font-medium text-secondary-content"
 			class:pointer-events-none={end === total}
 			>Next
 			<HoverBackground /></button
 		>
 	</div>
-	<div class="hidden md:flex sm:flex-1 sm:items-center sm:justify-between">
+	<div class="hidden md:flex sm:flex-1 sm:items-center sm:justify-between stwui-pagination-current">
 		<div>
 			<p class="text-sm text-secondary-content">
 				Showing
@@ -75,7 +75,7 @@
 					type="button"
 					on:click={onPreviousClick}
 					disabled={currentPage === 1}
-					class="relative overflow-hidden group inline-flex items-center rounded-l-md border border-border bg-surface px-2 py-2 text-sm font-medium text-secondary-content focus:z-20"
+					class="stwui-pagination-first relative overflow-hidden group inline-flex items-center rounded-l-md border border-border bg-surface px-2 py-2 text-sm font-medium text-secondary-content focus:z-20"
 					class:pointer-events-none={currentPage === 1}
 				>
 					<Icon data={chevron_left} />
@@ -85,7 +85,7 @@
 				{#each pagination as pageNumber}
 					{#if typeof pageNumber === 'string'}
 						<span
-							class="relative inline-flex items-center border border-border bg-surface px-4 py-2 text-sm font-medium text-secondary-content"
+							class="stwui-pagination-blank-page relative inline-flex items-center border border-border bg-surface px-4 py-2 text-sm font-medium text-secondary-content"
 						>
 							...
 						</span>
@@ -93,7 +93,7 @@
 						<button
 							type="button"
 							on:click={() => onPageClick(pageNumber)}
-							class="relative group overflow-hidden z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20"
+							class="stwui-pagination-page relative group overflow-hidden z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20"
 							class:bg-primary={pageNumber === currentPage}
 							class:border-primary={pageNumber === currentPage}
 							class:text-primary-content={pageNumber === currentPage}
@@ -111,7 +111,7 @@
 					type="button"
 					on:click={onNextClick}
 					disabled={end === total}
-					class="relative group overflow-hidden inline-flex items-center rounded-r-md border border-border bg-surface px-2 py-2 text-sm font-medium text-secondary-content focus:z-20"
+					class="stwui-pagination-last relative group overflow-hidden inline-flex items-center rounded-r-md border border-border bg-surface px-2 py-2 text-sm font-medium text-secondary-content focus:z-20"
 					class:pointer-events-none={end === total}
 				>
 					<Icon data={chevron_right} />
