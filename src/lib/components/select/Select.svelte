5af8851fc45f6c0390e36d299eead9ba3bd207ab
Craig Howell <howell.craig@gmail.com>
2023-11-13 12:25:46 -0500

    fix(Select): name now defaults to nanoid()

--- src/lib/components/select/Select.svelte
+++ src/lib/components/select/Select.svelte
@@ -11,8 +11,9 @@
 	import Portal from '../portal/Portal.svelte';
 	import Drawer from '../drawer';
 	import { twMerge } from 'tailwind-merge';
+	import { nanoid } from 'nanoid';
 
-	export let name: string;
+	export let name: string = nanoid();
 	export let error: string | undefined = undefined;
 	export let placeholder: string | undefined = undefined;
 	export let value: SelectOption | SelectOption[] | undefined = undefined;
